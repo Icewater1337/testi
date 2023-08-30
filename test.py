9623dcd7cf7e54d138fa97bd00225b08cce65dc2
Mathias Fuchs <fuchsmath+coding@gmail.com>
2023-08-30 16:08:21 +0200

    initial code

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-def sub(a, b):
-    return a - b
-def divide(a, b):
-    return a / b
+def add(a, b):
+    return a + b
 
 if __name__ == "__main__":
-    print("DIviding 5 and 3 gives:", divide(5, 3))
-    print("subtracting 5 and 3 gives:", sub(5, 3))
+    print("Adding 5 and 3 gives:", add(5, 3))
