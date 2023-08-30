12cfcc02768d692daad2c28253a5ef90ae53afd8
Mathias Fuchs <fuchsmath+coding@gmail.com>
2023-08-30 15:21:15 +0200

    Create rechner.py
    Rechner Basefile

---
+++ rechner.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+  print("ToDO: 3 python dateien erstellen:")
+  print("1. addierer.py")
+  print("2. multiplizierer.py")
+  print("3. subtrahierer.py")
+  print("Jede der Dateien soll die entsprechende Funktion beinhalten")
+  print("Die datei jeweils importieren in rechner.py")
+  print("In der main methode die entsprechende datei ausführen und testen")
